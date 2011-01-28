6fa28da26010a016cc2a0711ee4482be00f0e8f5
Mathieu Suen <mathk.sue@gmail.com>
2011-01-28 21:31:58 +0100

    Add auto-complete config

---
+++ lisp/auto-complete.el
@@ -0,0 +1,5 @@
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/package/auto-complete/dict")
+(require 'auto-complete-config)
+(ac-config-default)
+(setq ac-auto-start 4)
+(setq ac-auto-show-menu 0.8)
