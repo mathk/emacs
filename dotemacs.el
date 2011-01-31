2d355a60c151adf024909e839e42369bc3dc3e76
Mathieu Suen <mathieusuen@yahoo.fr>
2011-01-31 18:25:10 +0100

    Try to add org-mode

--- dotemacs.el
+++ dotemacs.el
@@ -10,6 +10,7 @@
 (load "defun")
 (load "global")
 
+;(vendor 'org-mode)
 (vendor 'magit)
 (vendor 'auto-complete)
 (vendor 'smart-tab)
---
+++ lisp/org-mode.el
@@ -0,0 +1,9 @@
+
+(add-to-list 'load-path "~/.emacs.d/package/org-mode/lisp")
+
+(require 'org)
+;(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;(setq org-log-done t)
+;(setq org-agenda-files (list "~/org/work.org"))
+
+
